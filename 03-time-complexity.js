5ca7d0ae134d896122669498253f69800d88fcca
Marco Arruda <marco.nc.arruda@gmail.com>
2021-09-01 13:19:02 -0300

    permMissingElem

--- 03-time-complexity.js
+++ 03-time-complexity.js
@@ -2,6 +2,15 @@ const frogJmp = (X, Y, D) => {
   return Math.ceil((Y-X) / D)
 }
 
+const permMissingElem = (A) => {
+  const check = new Array(A.length + 1)
+  for (let n of A) {
+    check[n-1] = true
+  }
+  return check.findIndex(n => !n) + 1
+}
+
 module.exports = {
-  frogJmp
+  frogJmp,
+  permMissingElem,
 }
--- __tests__/03-time-complexity.test.js
+++ __tests__/03-time-complexity.test.js
@@ -1,9 +1,16 @@
 const { describe, expect, test } = require('@jest/globals')
 
-const { frogJmp } = require('../03-time-complexity')
+const { frogJmp, permMissingElem } = require('../03-time-complexity')
 
 describe('frogJmp', () => {
   test('frogJmp', () => {
     expect(frogJmp(10, 85, 30)).toBe(3)
+  })
+})
+
+describe('permMissingElem', () => {
+  test('permMissingElem', () => {
+    const arr = [2, 3, 1, 5]
+    expect(permMissingElem(arr)).toBe(4)
   })
 })
